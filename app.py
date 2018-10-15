82b22e301892e1749f89ec9b2533aca5f28af128
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-10-15 15:08:00 +0530

    basic implementation

--- app.py
+++ app.py
@@ -7,28 +7,30 @@ from requests_oauthlib import OAuth1
 
 load_dotenv() # load environments
 
-APIKEY = os.getenv("APIKEY")
-APISECRET = os.getenv("APISECRET")
-TOKEN = os.getenv("TOKEN")
-TOKENSECRET = os.getenv("TOKENSECRET")
 
-day = 'today'
+def query(topic):
+	APIKEY = os.getenv("APIKEY")
+	APISECRET = os.getenv("APISECRET")
+	TOKEN = os.getenv("TOKEN")
+	TOKENSECRET = os.getenv("TOKENSECRET")
 
-LINK = f'https://api.twitter.com/1.1/search/tweets.json?q={day}&count=100&result_type=popular'
+	SEARCH_LINK = f'https://api.twitter.com/1.1/search/tweets.json?q={topic}&count=100&result_type=recent'
 
-auth = OAuth1(APIKEY, APISECRET, TOKEN, TOKENSECRET)
+	auth = OAuth1(APIKEY, APISECRET, TOKEN, TOKENSECRET)
 
-r = requests.get(LINK, auth=auth)
+	r = requests.get(SEARCH_LINK, auth=auth)
 
-print(r)
-rjson = r.json()
+	if r.status_code == 200:
+		rjson = r.json()
 
-# print(json.dumps(rjson, indent=4, sort_keys=True))
+		# print(json.dumps(rjson, indent=4, sort_keys=True))
 
-statuses = rjson['statuses']
-# print( json.dumps(statuses, indent=4))
-print(f"Number of statuses : {len(statuses)}")
+		statuses = rjson['statuses']
+		# print( json.dumps(statuses, indent=4))
+		print(f"Number of statuses : {len(statuses)}")
 
-for tweet in statuses:
-	text = tweet['text']
-	print(text)
+		print(f"{topic} : {sum([tweet['text'].count(topic) for tweet in statuses])}")
+
+
+query('happy')
+query('sad')
